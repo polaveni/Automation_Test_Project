96c6a4d77fdd8dd5a184b5cd1f3afd572a5d9dbb
ShekerPolaveni <shekerp55@gmail.com>
2021-03-29 09:51:50 +0530

    Created First and SecondPrograme java class file

--- main/testing/SecondProgram.java
+++ main/testing/SecondProgram.java
@@ -1,22 +1,14 @@
 package testing;
 
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
-public class First {
-	
-	
+public class SecondProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hi");
-		System.out.println("Hi Kiran");
-		
-		//Homegenious Elements
-		
-        Set ts=new TreeSet();
+	Set ts=new TreeSet();
 		
 		ts.add("shekar");
 		ts.add("Raju");
@@ -32,11 +24,6 @@ public class First {
 	//	TreeMap tm=new TreeMap();
 		
 	
-		
-		
-		
-		
-		
 
 	}
 
